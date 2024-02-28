b5b8cf993f661cc86e21b4edd624f6f5e9c09267
Nicolas Ottati <ottatinicolas91@gmail.com>
2024-02-28 01:25:24 +0100

    Ok, finished stimulus but have to add the insert or switch to display on toggle. Off to sleep

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,9 @@
 import { Application } from "@hotwired/stimulus"
+import CardController from "../controllers/item_card_controller.js";
+
 
 const application = Application.start()
+application.register("card-body", CardController);
 
 // Configure Stimulus development experience
 application.debug = false
