0375c1142585ba6b246b96b6c2da28a3a3991ca1
valeria lezcano <msvalerialg@gmail.com>
2024-02-29 15:53:57 +0100

    form done for the owner side

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,23 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+
   end
 
   def show
     @item = Item.find(params[:id])
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to item_path(@item), notice: 'Item created!'
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :quantity, :price, :category_id, :photo)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :category
   has_one_attached :photo
+  
 end
