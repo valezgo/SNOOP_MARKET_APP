55b5bfbd05f7d71a4db47666b67d3a2bd7bc643f
valeria lezcano <msvalerialg@gmail.com>
2024-03-01 17:16:23 +0100

    Remove unused Stimulus controller

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,4 @@
 import { Application } from "@hotwired/stimulus"
-import CardController from "../controllers/item_card_controller.js";
-
 
 const application = Application.start()
 application.register("card-body", CardController);
--- app/javascript/controllers/item_card_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  static targets = ["card-body"];
-
-  toggle() {
-    this.bodyTarget.classList.toggle("d-none");
-  }
-}
