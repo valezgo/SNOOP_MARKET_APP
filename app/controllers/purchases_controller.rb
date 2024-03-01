d82caee3e7286fc6f5d926f14ab65ecda3ce620a
Nicolas Ottati <ottatinicolas91@gmail.com>
2024-03-01 13:03:02 +0100

    Implemented dynamic content for the checkout

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,10 @@ class PurchasesController < ApplicationController
   def new
     @purchase = Purchase.new
     @selected_items = Selecteditem.where(user: current_user)
+    @total_price = 0
+    @selected_items.each do |selected_item|
+      @total_price += selected_item.quantity * selected_item.item.price
+    end
   end
 
   def create
